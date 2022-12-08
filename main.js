fd6f1157b5ca6134533c7eeb1b29882e302157c8
Jimy Emanuel Diaz Gordillo <jimmy.054@hotmail.com>
2022-12-07 18:46:00 -0600

    Mobile Menu finishied

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 var navEmail = document.querySelector(".navbar-email");
 var desktopMenu = document.querySelector(".desktop-menu");
-navEmail.addEventListener('click',toggleDesktopMenu);
+var menuHamIcon = document.querySelector(".menu");
+var mobileMenu = document.querySelector(".mobile-menu")
 
+
+navEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
